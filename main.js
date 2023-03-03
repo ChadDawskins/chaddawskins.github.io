ecf368bd21732544b469b4f4be8c53e023be929e
ChadDawskins <126297241+ChadDawskins@users.noreply.github.com>
2023-03-03 13:57:30 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ form.addEventListener("submit", function(event) {
 	// get the text input value
 	const textInput = document.getElementById("text-input").value;
 	
-	fetch('https://api.openai.com/v1/completions', {
+	fetch('https://api.openai.com/v1/chat/completions', {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
@@ -17,19 +17,19 @@ form.addEventListener("submit", function(event) {
 		},
 
 		body: JSON.stringify({
-			'model': 'text-davinci-003',
-			'prompt': 'You are 22 years old and live in Houston. You are a little snarky and like to make jokes. Sami is your sister. Parker is your brother. Mark is your roommate. Julia is your girlfriend. Julia lives in Austria.  Reply to this prompt: ' + textInput,
-			'temperature': 0.3,
-			'max_tokens': 45,
+			'model': 'gpt-3.5-turbo',
+			"messages": [{"role": "system", "content": "You are a 22 year old college student named Dylan."},{"role":"user","content": "You attend Rice University in Houston. You are a little snarky and like to make jokes. You will sometimes say, 'uh..' or 'well...' at the beginning of sentences. Sami is your sister who lives in San Jose, CA. Nobody likes Sami. Parker is your brother who lives in Boston, MA. Mark is your roommate. Julia is your girlfriend. Julia lives in Austria. You like sliced bread but Julia does not. Reply to this prompt:" + textInput}],
+			'temperature': 0.5,
+			'max_tokens': 100,
 			'top_p': 1,
 			'frequency_penalty': 0.25,
-			'presence_penalty': 0.85
+			'presence_penalty': 0.6
 		})
 	})
 	.then(function(u){ return u.json();}
 		  ).then(
 			function(json){
-			  const dylan_response = json['choices']['0']['text'];
+			  const dylan_response = json['choices']['0']['message']['content'];
 
 			  fetch('https://api.elevenlabs.io/v1/text-to-speech/tpXFK4EOxoKAvgG6zSLF', {
 					method: 'POST',
